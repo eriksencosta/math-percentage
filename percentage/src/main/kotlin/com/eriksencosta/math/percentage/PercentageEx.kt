638c9fc7c68062581c4e49d7ee01a65824f39dee
Eriksen Costa <eriksencosta@gmail.com>
2024-09-29 18:56:59 -0300

    Move functions between files

--- percentage/src/main/kotlin/com/eriksencosta/math/percentage/PercentageEx.kt
+++ percentage/src/main/kotlin/com/eriksencosta/math/percentage/PercentageEx.kt
@@ -1,6 +1,24 @@
 package com.eriksencosta.math.percentage
 
 /**
+ * Sums all [Percentage] elements in the collection. The rounding strategy of the first element is preserved in the
+ * resulting [Percentage].
+ *
+ * @receiver [Iterable]
+ * @return The resulting [Percentage].
+ */
+public fun Iterable<Percentage>.sum(): Percentage = (this as Collection<Percentage>).sum()
+
+/**
+ * Sums all [Percentage] elements in the collection. The rounding strategy of the first element is preserved in the
+ * resulting [Percentage].
+ *
+ * @receiver [Collection]
+ * @return The resulting [Percentage].
+ */
+public fun Collection<Percentage>.sum(): Percentage = sumOf { it.value }.percent(this.first().rounding)
+
+/**
  * Returns a detailed string representation of the object.
  *
  * @receiver [Percentage]
